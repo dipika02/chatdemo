548b7231ea0b2422fab480f567d68746240ef01e
dipika <dtiwane47@gmail.com>
2016-08-01 19:04:27 +0530

    create a new group

--- socket/socket.js
+++ socket/socket.js
@@ -1,14 +1,23 @@
 module.exports = function (io,groups){
 	
 	var chat = io.of('/grouplist').on('connection',function(socket){
-		console.log ("connection Estabilished on the server !!!");
+		console.log ("connection Estabilished on the server @!!!");
+			 socket.emit('groupupdate', JSON.stringify(groups));
 
 
 		socket.on('newgroup',function(data){
 			 groups.push(data);
-			 socket.broadcast.emit('gropupdate',JSON.stringify(groups));
-			 socket.emit('gropupdate',JSON.stringify(groups));
+			 // console.log("data"+data);
+			 socket.broadcast.emit('groupupdate', JSON.stringify(groups));
+			 socket.emit('groupupdate', JSON.stringify(groups));
+			 console.log("JSON.stringify(groups)"+JSON.stringify(groups));
+			 // console.log("data1111"+data);
 		})
+
+
+
+
+
 
 	}) 
 }
